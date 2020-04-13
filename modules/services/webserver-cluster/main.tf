d0fe6e3bed4ea4e67e594946a991c76b3b491453
Bessenyei Csilla <bessenyei.csilla@gmail.com>
2020-04-13 17:04:25 +0100

    separate to module and live

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -82,21 +82,28 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
-
-  ingress {
-    from_port = local.http_port
-    to_port = local.http_port
-    protocol = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
-
-  egress {
-    from_port = local.any_port
-    to_port = local.any_port
-    protocol = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
 }
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 
 resource "aws_lb_target_group" "asg" {
   name = var.cluster_name
@@ -129,31 +136,31 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-csillab"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "eu-west-1"
-    dynamodb_table = "terraform-up-and-running-locks-csillab"
-    encrypt = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-up-and-running-state-csillab"
+#    key = "stage/services/webserver-cluster/terraform.tfstate"
+#    region = "eu-west-1"
+#    dynamodb_table = "terraform-up-and-running-locks-csillab"
+#    encrypt = true
+#  }
+#}
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-  config = {
-    bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
-    region = "eu-west-1"
-  }
-}
+#data "terraform_remote_state" "db" {
+#  backend = "s3"
+#  config = {
+#    bucket = var.db_remote_state_bucket
+#    key = var.db_remote_state_key
+#    region = "eu-west-1"
+#  }
+#}
 
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
+#    db_address = data.terraform_remote_state.db.outputs.address
+#    db_port = data.terraform_remote_state.db.outputs.port
   }
 }
--- modules/services/webserver-cluster/user-data.sh
+++ modules/services/webserver-cluster/user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat > index.html <<EOF
 <h1>Hello, World</h1>
-<p>DB addr: ${db_address}</p>
-<p>DB port: ${db_port}</p>
+#<p>DB addr: ${db_address}</p>
+#<p>DB port: ${db_port}</p>
 
 EOF
 
